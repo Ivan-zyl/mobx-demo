13243e69bd2db4d7941494c7a73e45e32276bad3
zyl <wb-zyl250943@alibaba-inc.com>
2018-09-20 16:22:12 +0800

    dotdotdot

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -1,35 +1,79 @@
-/**
- * @description index page
- * @author zyl
- * @createAt 2018/8/19
- */
-
 import React, { Component } from 'react';
-import { observer, inject } from "mobx-react";
+import Truncate from 'react-truncate';
 
-import Header from './header';
+class ReadMore extends Component {
+  constructor(...args) {
+    super(...args);
 
-@inject('home') @observer
-export default class Index extends Component {
-  constructor(props) {
-    super(props);
+    this.state = {
+      expanded: false,
+      truncated: false,
+      search: ''
+    };
+
+    this.handleTruncate = this.handleTruncate.bind(this);
+    this.toggleLines = this.toggleLines.bind(this);
   }
 
-  async componentDidMount() {
-    const resp = await this.props.home.fetchUser();
-    this.props.home.changeAuthor(resp.data);
+  handleTruncate(truncated) {
+    if (this.state.truncated !== truncated) {
+      this.setState({
+        truncated
+      });
+    }
+  }
+
+  toggleLines(event) {
+    event.preventDefault();
+
+    this.setState({
+      expanded: !this.state.expanded
+    });
   }
 
   render() {
+
+    const {
+      expanded,
+      truncated,
+      search
+    } = this.state;
+    let children = 'nihao,dfhkal,asld/wer.asdfnihao,dfhkal,asld/wer asdfasdfasdf';
+    let content;
+    if (search) {
+      content = children.split(search);
+      content = content.map((item, index) => {
+        return (
+          <span key={index}>
+            {item}
+            <i style={{ color: 'yellow' }}>{search}</i>
+          </span>
+        )
+      })
+    } else {
+      content = children;
+    }
     
-    const { author } = this.props.home;
 
     return (
       <div>
-        <div>mobx demo</div>
-        <div>author: {author}</div>
-        <Header {...this.props} />
+        <input value={this.state.search} onChange={e => this.setState({ search: e.target.value })} />
+        <div style={{ width: 200, wordBreak: 'break-all' }}>
+          <Truncate
+            lines={!expanded && 3}
+            ellipsis={(
+              <a href='#' onClick={this.toggleLines}>Read more</a>
+            )}
+          >
+            {content}
+          </Truncate>
+          {expanded && (
+            <span> <a href='#' onClick={this.toggleLines}>Show less</a></span>
+          )}
+        </div>
       </div>
-    )
+    );
   }
 }
+
+export default ReadMore;
